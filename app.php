47f072a2ce47ac18e6d97205c1e80eff8939b45c
MRRazvi <mrrazvi@icloud.com>
2023-11-10 01:38:13 +0500

    wip

--- app.php
+++ app.php
@@ -4,8 +4,19 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/../wp-load.php';
 
 // load .env file
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-// configuration for access keys, fields mappings
-$config = [
-    'space_id' => ''
+// fields mapping
+$fields = [
+    'post_title'        => 'title',
+    'post_name'         => 'slug',
+    'post_excerpt'      => 'description',
+    'post_content'      => 'contentBlock1',
+    'post_thumbnail'    => 'headerImage',
+    'post_tags'         => 'keywords',
+    'post_status'       => 'publish',
+    'post_type'         => 'post',
 ];
+
+// get all the posts from contentful
