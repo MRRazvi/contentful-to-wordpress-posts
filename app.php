6e9c88cea14df625e73fc4be2192dcfc5e9803a9
MRRazvi <mrrazvi@icloud.com>
2023-11-10 01:29:50 +0500

    wip

---
+++ app.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../wp-load.php';
+
+// load .env file
+
+// configuration for access keys, fields mappings
+$config = [
+    'space_id' => ''
+];
